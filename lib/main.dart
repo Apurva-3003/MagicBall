b2c774eac7cc69b7c5de2c0774b9cf15940bd6cb
Apurva Agrawal <apurva.agrawal3003@gmail.com>
2021-09-01 14:58:00 -0400

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.purpleAccent,
+        appBar: AppBar(
+          title: Center(child: Text('Magic 8 Ball')),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: MagicBall(),
       ),
+    ),
+  );
+}
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+
+  int update = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: TextButton(
+            child: Image.asset('images/ball$update.png'),
+            onPressed: (){
+              setState(() {
+                update = Random().nextInt(5)+1;
+              });},
+          ),
+        ),
+      ],
     );
+  }
+}
